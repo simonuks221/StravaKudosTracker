1e01ee9258804fe9ae92fca01b39ff9222948235
simonuks221 <simonasriauka@gmail.com>
2021-07-07 19:30:03 +0300

    Stuff

--- src/components/KudosChart.js
+++ src/components/KudosChart.js
@@ -1,12 +1,14 @@
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { useEffect } from 'react';
 
-const KudosChart = (kudosData) => {
+const KudosChart = ({kudosData, activityCount}) => {
     const CustomTooltip = ({active, payload, label}) => {
         if (active && payload && payload.length) {
             return (
                 <div className="chart-tooltip">
-                    <p>{`User name: ${label}, ${payload[0].payload.surname}`}</p>
-                    <p>{`Amount of Kudos given: ${payload[0].value}`}</p>
+                    <p>{`#${kudosData.findIndex(element => element.name === payload[0].payload.name) + 1} ${label}, ${payload[0].payload.surname}`}</p>
+                    <hr className = 'solid'/>
+                    <p>{`Kudos given: ${payload[0].value}`}</p>
                 </div>
             )
         }else{
@@ -16,12 +18,13 @@ const KudosChart = (kudosData) => {
 
     return (
       <div className = 'secondary-panel'>
-        <ResponsiveContainer width="100%" height={kudosData.kudosData.length * 75}>
+        {kudosData.length === 0? <p>Looks like you don't have any Kudos given to you :(</p>:
+        <ResponsiveContainer width="100%" height={kudosData.length * 75}>
           <BarChart
       layout= 'vertical'
       width={800}
-      height={kudosData.kudosData.length * 75}
-      data={kudosData.kudosData}
+      height={kudosData.length * 75}
+      data={kudosData}
       margin={{
         top: 5,
         right: 30,
@@ -29,15 +32,16 @@ const KudosChart = (kudosData) => {
         bottom: 5,
       }}>
         <CartesianGrid strokeDasharray="6 6" horizontal = {false} />
-        <XAxis type="number"/>
+        <XAxis type="number" dataKey = "kudos"/>
         <YAxis type="category" dataKey="name" />
-        <Bar dataKey="kudos" barSize = {50} fill={getComputedStyle(document.documentElement).getPropertyValue('--primaryColor')} />
-        <Tooltip content={<CustomTooltip/>}/>
+        <Bar name = {`Number of Kudos given across ${activityCount} ${activityCount === 1? 'activity': 'activities'}`} dataKey="kudos" barSize = {50} fill={getComputedStyle(document.documentElement).getPropertyValue('--primaryColor')} />
+        <Tooltip content={<CustomTooltip/>} cursor = {{fill: 'rgb(255, 255, 255, 0.6)'}}/>
         <Legend/>
     </BarChart>
     </ResponsiveContainer>
+}
       </div>
-      
+    
     )
 }
 
--- src/components/LoggedInPage.js
+++ src/components/LoggedInPage.js
@@ -6,6 +6,7 @@ import KudosChart from "./KudosChart.js"
 const LoggedInPage = () => {
     const context = useContext(AuthContext)
     const [kudosData, changeKudosData] = useState()
+    const [activityCount, changeActivityCount] = useState(0)
     const [dataQueried, changeDataQueried] = useState(false)
 
     const GetAllAthleteActivities = async () => {
@@ -21,15 +22,18 @@ const LoggedInPage = () => {
                 return data
             }else{
                 if(response.status === 429){
-                    alert('Too many requests, please try again later')
+                    alert('Too many requests, please try again later 1')
+                    return null
                 }else{
                     console.log('Couldnt get all activities: ', response)
+                    return null
                 }
                
             }
         }
         catch(e){
             console.log('Error encountered while geeting all activities: ', e)
+            return null
         }
     }
 
@@ -45,21 +49,25 @@ const LoggedInPage = () => {
                 return data;
             }else{
                 if(response.status === 429){
-                    alert('Too many requests, please try again later')
+                    alert('Too many requests, please try again later 2')
+                    return null
                 }else{
                     console.log('Couldnt get kudoers of the activity: ', response)
+                    return null
                 }
                 
             }
         }
         catch(e){
             console.log('Error encountered while geeting all kudoers: ', e)
+            return null
         }
     }
 
     const GetTopKudoers = async () => {
         const topKudoers = []
         const allActivityData = await GetAllAthleteActivities()
+        const activitiesCounted = 0
         if(allActivityData){
             allActivityData.forEach(async activity => {
                 const activityKudoers = await GetAllActivityKudoers(activity.id)
@@ -72,74 +80,117 @@ const LoggedInPage = () => {
                         else{ //Kudoer exists
                             topKudoers[index] = {...topKudoers[index], kudos: topKudoers[index].kudos + 1}
                         }
-                        
                     }
+                    activitiesCounted++
+                }
+                else{
+                    throw('Didnt get break the loop')
                 }
             })
+            changeActivityCount(activitiesCounted)
+            changeKudosData(topKudoers)
+            changeDataQueried(true)
         }
-        changeKudosData(topKudoers)
-        changeDataQueried(true)
     }
 
     const GetDummyData = () => {
         const data = [
             {
-                name: 'Rusne',
-                surname: 'Baltr',
+                name: 'Petras1',
+                surname: 'Petrauskas1',
                 kudos: 4,
               },
               {
-                name: 'Page B',
-                surname: 'Baltr',
+                name: 'Antanas11',
+                surname: 'Petrauskas2',
                 kudos: 5,
               },
               {
-                name: 'Page C',
-                surname: 'Baltr',
-                kudos: 3,
+                name: 'Petras2',
+                surname: 'Petrauskas3',
+                kudos: 7,
               },
               {
-                name: 'Page C',
-                surname: 'Baltr',
-                kudos: 3,
+                name: 'Antanas2',
+                surname: 'Petrauskas4',
+                kudos: 2,
               },
               {
-                name: 'Page C',
-                surname: 'Baltr',
-                kudos: 3,
+                name: 'Petras3',
+                surname: 'Petrauskas5',
+                kudos: 1,
               },
               {
-                name: 'Page C',
-                surname: 'Baltr',
+                name: 'Antanas3',
+                surname: 'Petrauskas6',
                 kudos: 3,
               }
         ]
-        changeKudosData(data)
+        changeActivityCount(7)
+        changeKudosData(Array.from(data))
         changeDataQueried(true)
     }
 
     const ClearData = () => {
+        changeActivityCount(0)
         changeKudosData({})
         changeDataQueried(false)
     }
 
     const SortArray = (arrayToSort) => { //Sorts by kudos amount
-        return arrayToSort.sort((a, b) => { return b.kudos - a.kudos})
+        if(arrayToSort){
+            console.log(arrayToSort, ' ', typeof(arrayToSort))
+            return arrayToSort.sort((a, b) => { return b.kudos - a.kudos})
+        }
+        return ''
+    }
+
+    const Deauthorize = async () => {
+        try{
+            const response = await fetch(`https://www.strava.com/oauth/deauthorize?access_token${context.auth.data.access_token}/kudos`, {
+                method: 'POST',
+                headers: {
+                    "Authorization": `Bearer ${context.auth.data.access_token}`
+                }})
+            if(response.ok){
+                context.changeAuth({})
+                context.changeState('preLogin')
+                return
+            }else{
+                if(response.status === 429){
+                    alert('Too many requests, please try again later 3')
+                    
+                }else{
+                    console.log('Couldnt deauthorize: ', response)
+                }
+                
+            }
+        }
+        catch(e){
+            console.log('Error encountered while deauthorizing: ', e)
+        }
     }
 
     return (
         <div className = 'page logged-in'>
             <Container fluid>
                 <Row>
+                    <Col className = 'mb-2'>
+                    {context.auth.data? <h2 className = 'secondary-panel text-center'>{`Hello ${context.auth.data.athlete.firstname} ${context.auth.data.athlete.lastname}`}</h2>: ''}
+                    </Col>
+                </Row>
+                <Row>
                     <Col md={10}>
-                        {dataQueried? <KudosChart kudosData = {SortArray(kudosData)}/>: <div className = 'chart-placeholder secondary-panel'>
+                        {dataQueried? <KudosChart activityCount = {activityCount} kudosData = {SortArray(kudosData)}/>: <div className = 'chart-placeholder secondary-panel'>
                                 <h4>Kudos data will show up here after you press the button on the right</h4>
                             </div>}
                     </Col>
                     <Col md={2}>
                         <button className = 'link-button mb-5' onClick = {GetTopKudoers}>Get activities</button>
                         <button className = 'link-button mb-5' onClick = {GetDummyData}>Get dummy data</button>
-                        <button className = 'link-button' onClick = {ClearData}>Clear data</button>
+                        {dataQueried? <button className = 'link-button mb-5' onClick = {ClearData}>Clear data</button>: ''}
+                        <button className = 'link-button' onClick = {Deauthorize}>Log out</button>
+
                     </Col>
                 </Row>
             </Container>
